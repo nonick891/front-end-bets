331e222dc58716a81691b22fd537a21b7d3e6253
Nikita Volchok <volue.nik@gmail.com>
2020-03-21 14:03:47 +0300

    events: added triple inline.

--- src/components/ui/events/detail/game.vue
+++ src/components/ui/events/detail/game.vue
@@ -9,6 +9,10 @@
         v-else-if="getMarketType(odd) === 'OverUnder'"
         :odd="odd"
       />
+      <triple-inline
+        v-else-if="odd.results.length === 3"
+        :odd="odd"
+      />
       <default-table
         v-else
         :odd="odd"
@@ -22,11 +26,12 @@ import { get } from 'lodash'
 import defaultTable from './table/default.vue'
 import overUnderTable from './table/over-under.vue'
 import correctScore from './table/correct-score.vue'
+import tripleInline from './table/triple-inline.vue'
 export default {
   props: {
     odd: Object
   },
-  components: { defaultTable, overUnderTable, correctScore },
+  components: { defaultTable, overUnderTable, correctScore, tripleInline },
   methods: {
     getMarketType(odd) {
       return get(odd, 'grouping.parameters.marketType', '')
---
+++ src/components/ui/events/detail/table/triple-inline.vue
@@ -0,0 +1,30 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+      <tr>
+        <th
+          colspan="3"
+          class="text-left"
+        >
+          {{ odd.name.value }}
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>{{ odd.results[0].name.value }} <b>{{ odd.results[0].odds }}</b></td>
+        <td>{{ odd.results[1].name.value }} <b>{{ odd.results[1].odds }}</b></td>
+        <td>{{ odd.results[2].name.value }} <b>{{ odd.results[2].odds }}</b></td>
+      </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+<script>
+export default {
+  props: {
+    odd: Object
+  }
+}
+</script>
