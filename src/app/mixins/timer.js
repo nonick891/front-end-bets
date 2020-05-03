704d3c2af09178d0be93ef015ec8b238249d6bad
Nikita Volchok <volue.nik@gmail.com>
2020-05-03 12:58:03 +0300

    scoreboard: - added timer break; - changed timer showing condition.

--- src/app/mixins/timer.js
+++ src/app/mixins/timer.js
@@ -12,34 +12,48 @@ const getStringTimer = seconds => {
     .format('mm:ss')
 };
 
+const getSeconds = (startDate, offsetMinutes) => {
+  let current = moment.utc(),
+      end = getMomentAdd(startDate, offsetMinutes);
+   return current.diff(end, 'seconds');
+};
+
+const isHalfBreak = (startDate) => {
+  let endFirst = getMomentAdd(startDate, 45),
+      startSecond = getMomentAdd(startDate, 60);
+      return moment.utc().isBetween(endFirst, startSecond);
+};
+
 export default {
   data: () => ({
     gameTimer: '00:00'
   }),
   computed: {
-    ...mapState('scoreboard', ['timer', 'dateTimer']),
+    ...mapState('scoreboard', ['timer', 'dateTimer', 'period']),
   },
   watch: {
     timer(value) {
       if (value.base && value.running && value.visible) {
         let timerId = null;
         timerId = setInterval(() => {
-          let current = moment.utc(),
-            endFH = getMomentAdd(this.dateTimer.startDate, 45),
-            endSH = getMomentAdd(this.dateTimer.startDate, 105),
-            endFirstSeconds = current.diff(endFH, 'seconds'),
-            endSecondSeconds = current.diff(endSH, 'seconds'),
-            finalSeconds = (45*60) + (
+          if (isHalfBreak(this.dateTimer.startDate)) {
+            this.gameTimer = 'Break';
+            return false;
+          }
+          let firstHalfPassed = this.period === '1H' ? 0 : 45,
+              endFirstSeconds = getSeconds(this.dateTimer.startDate, 45),
+              endSecondSeconds = getSeconds(this.dateTimer.startDate, 105),
+              finalSeconds = (45 + firstHalfPassed) * 60 + (
                 endFirstSeconds < 0
                   ? endFirstSeconds
                   : endSecondSeconds
               );
           this.gameTimer = getStringTimer(finalSeconds);
-          if (finalSeconds < 0) {
+          if (endSecondSeconds > 0 && endFirstSeconds > 0) {
             this.gameTimer = 'Finished';
             clearInterval(timerId);
           }
-        }, 1000);
+        }, 980);
       }
     }
   }
