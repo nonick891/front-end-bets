84f3805365c16077c2e6cc8166d9857136928e88
Nikita Volchok <volue.nik@gmail.com>
2020-05-27 23:29:12 +0300

    account: - added regular template.

--- src/components/pages/account/regular.vue
+++ src/components/pages/account/regular.vue
@@ -6,13 +6,39 @@
       fluid
       class="pa-0"
     >
-      account
-      <v-btn to="/account/admin">Admin page</v-btn>
+      <line-btn
+        :icon="'credit'"
+        :color="'transparent'"
+        :text="'Credit: 234 EUR'"
+      />
+      <line-btn
+        :icon="'lock'"
+        :color="'#4C4C4C'"
+        :text="'Change password'"
+      />
+      <line-btn
+        :icon="'logout'"
+        :color="'#4C4C4C'"
+        :text="'Logout'"
+      />
+      <!-- START REMOVABLE TEMPLATE -->
+      <v-row
+        class="pt-5"
+        justify="center"
+        no-gutters
+      >
+        <v-btn
+          color="red"
+          to="/account/admin"
+        >Admin page</v-btn>
+      </v-row>
+      <!-- END REMOVABLE TEMPLATE -->
     </v-container>
   </v-content>
 </template>
 <script>
+import lineBtn from '../../ui/account/line-button.vue'
 export default {
-  data: () => ({})
+  components: { lineBtn }
 }
 </script>
---
+++ src/components/ui/account/line-button.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row
+    no-gutters
+    class="py-2 px-1"
+  >
+    <v-card
+      :color="color"
+      width="100%"
+      class="line-btn-text pa-3"
+    >
+      <v-icon
+        :size="size"
+      >
+        $vuetify.icons.{{ icon }}
+      </v-icon>
+      {{ text }}
+    </v-card>
+  </v-row>
+</template>
+<script>
+export default {
+  props: [
+    'text',
+    'icon',
+    'color',
+    'size'
+  ]
+}
+</script>
+<style>
+  .line-btn-text {
+    font-size: 18px;
+    line-height: 21px;
+  }
+  .line-btn-text.v-card:not(.v-sheet--tile):not(.v-card--shaped) {
+    border-radius: 8px;
+  }
+</style>
